fbd5e63d0602ba968687588682ee77c5348fd7fa
Francesco Alagna <s265350@studenti.polito.it>
2023-03-03 10:58:09 +0100

    startfiles

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker-compose up --build
+# The --build flag is useful when you’ve run this command before and want to build new image
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker-compose down -V
+# The -V flag (volumes) ensures that the containers and attached volumes are shut down
