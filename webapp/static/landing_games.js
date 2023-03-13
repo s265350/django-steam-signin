485916a1f2b6ba5cb2933892545d3adcf090dd2b
Francesco Alagna <s265350@studenti.polito.it>
2023-03-13 12:55:52 +0000

    games list and scroll

--- webapp/static/landing_games.js
+++ webapp/static/landing_games.js
@@ -1,15 +1,67 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+const group = document.getElementById('games_group');
 
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
+const srcs = [
+    '//via.placeholder.com/300x300/31f',
+    '//via.placeholder.com/300x300/aba',
+    '//via.placeholder.com/300x300/fc0',
+    '//via.placeholder.com/300x300/e66',
+    '//via.placeholder.com/300x300/7d2',
+    '//via.placeholder.com/300x300',
+    '//via.placeholder.com/300x300/aba',
+    '//via.placeholder.com/300x300/fc0',
+    '//via.placeholder.com/300x300/31f',
+    '//via.placeholder.com/300x300/7d2',
+    '//via.placeholder.com/300x300/e66',
+    '//via.placeholder.com/300x300',
+    '//via.placeholder.com/300x300/e66',
+    '//via.placeholder.com/300x300/7d2',
+    '//via.placeholder.com/300x300/31f',
+    '//via.placeholder.com/300x300/aba',
+    '//via.placeholder.com/300x300/fc0',
+    '//via.placeholder.com/300x300',
+    '//via.placeholder.com/300x300/31f',
+    '//via.placeholder.com/300x300/aba',
+    '//via.placeholder.com/300x300/fc0',
+    '//via.placeholder.com/300x300/e66',
+    '//via.placeholder.com/300x300/7d2',
+    '//via.placeholder.com/300x300',
+    '//via.placeholder.com/300x300/aba',
+    '//via.placeholder.com/300x300/fc0',
+    '//via.placeholder.com/300x300/31f',
+    '//via.placeholder.com/300x300/7d2',
+    '//via.placeholder.com/300x300/e66',
+    '//via.placeholder.com/300x300',
+    '//via.placeholder.com/300x300/e66',
+    '//via.placeholder.com/300x300/7d2',
+    '//via.placeholder.com/300x300/31f',
+    '//via.placeholder.com/300x300/aba',
+    '//via.placeholder.com/300x300/fc0',
+    '//via.placeholder.com/300x300',
+];
+
+srcs.forEach((src) => {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item col-2 bg-transparent border-0');
+    group.appendChild(li);
+
+    const img = document.createElement('img');
+    img.setAttribute('class', 'img-fluid rounded');
+    img.setAttribute('src', src);
+    li.appendChild(img);
 })
+
+window.addEventListener('load', () => {
+    var last = -1;
+    var goback = false;
+    self.setInterval(() => {
+        if (last === group.scrollLeft) {
+            goback = !goback;
+        }
+        last = group.scrollLeft;
+        if (goback) {
+            group.scrollTo(group.scrollLeft - 1, 0);
+        } else {
+            group.scrollTo(group.scrollLeft + 1, 0);
+        }
+    }, 15);
+  });
